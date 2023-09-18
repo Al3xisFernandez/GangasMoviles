07e16860c3c6986c91414add2ca914421b8aed08
Al3xisFernandez <alexxxfernandez50@gmail.com>
2023-09-18 03:19:23 -0300

    finishedFrontend

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,105 +1,44 @@
-// let currentSlide = 0;
-// const slides = document.querySelectorAll('.slider img');
-// const prevBtn = document.getElementById('prevBtn');
-// const nextBtn = document.getElementById('nextBtn');
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
 
-// // function showSlide(n) {
-// //     slides[currentSlide].classList.remove('active'); // Quita la clase 'active' de la imagen actual
-// //     currentSlide = (n + slides.length) % slides.length;
-// //     slides[currentSlide].classList.add('active'); // Agrega la clase 'active' a la nueva imagen
-// //     slides[currentSlide].style.display = 'none';
-// //     currentSlide = (n + slides.length) % slides.length;
-// //     slides[currentSlide].style.display = 'block';
-    
-// //     /* ... Tu código JavaScript existente ... */
-    
-// // }
-
-// // function prevSlide() {
-// //     showSlide(currentSlide - 1);
-// // }
-
-// // function nextSlide() {
-// //     showSlide(currentSlide + 1);
-// // }
-// function showSlide(n) {
-//     slides[currentSlide].classList.remove('active'); // Quita la clase 'active' de la imagen actual
-//     currentSlide = (n + slides.length) % slides.length;
-//     slides[currentSlide].classList.add('active'); // Agrega la clase 'active' a la nueva imagen
-// }
-
-// function prevSlide() {
-//     showSlide(currentSlide - 1);
-// }
-
-// function nextSlide() {
-//     showSlide(currentSlide + 1);
-// }
-
-
-// prevBtn.addEventListener('click', prevSlide);
-// nextBtn.addEventListener('click', nextSlide);
-
-// const interval = 3000; 
-
-// function autoRotate() {
-//     nextSlide();
-// }
-
-// let autoRotation = setInterval(autoRotate, interval);
-
-// prevBtn.addEventListener('click', () => {
-//     clearInterval(autoRotation);
-//     autoRotation = setInterval(autoRotate, interval);
-// });
-
-// nextBtn.addEventListener('click', () => {
-//     clearInterval(autoRotation);
-//     autoRotation = setInterval(autoRotate, interval);
-// });
-
-// showSlide(currentSlide);
-
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slider img');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-
-function showSlide(n) {
-    slides[currentSlide].classList.remove('active'); // Quita la clase 'active' de la imagen actual
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active'); // Agrega la clase 'active' a la nueva imagen
-}
-
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
+let slideIndex = 0;
+let intervalId;
 
 function nextSlide() {
-    showSlide(currentSlide + 1);
+    slides[slideIndex].classList.remove("active");
+    if (slideIndex < slides.length - 1) {
+        slideIndex++;
+    } else {
+        slideIndex = 0;
+    }
+    slides[slideIndex].classList.add("active");
 }
 
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
+prevButton.addEventListener("click", () => {
+    slides[slideIndex].classList.remove("active");
+    if (slideIndex > 0) {
+        slideIndex--;
+    } else {
+        slideIndex = slides.length - 1;
+    }
+    slides[slideIndex].classList.add("active");
+});
 
-const interval = 3000; 
-
-function autoRotate() {
+nextButton.addEventListener("click", () => {
     nextSlide();
-}
-
-let autoRotation = setInterval(autoRotate, interval);
-
-prevBtn.addEventListener('click', () => {
-    clearInterval(autoRotation);
-    autoRotation = setInterval(autoRotate, interval);
 });
 
-nextBtn.addEventListener('click', () => {
-    clearInterval(autoRotation);
-    autoRotation = setInterval(autoRotate, interval);
+intervalId = setInterval(nextSlide, 3000);
+
+slider.addEventListener("mouseenter", () => {
+    clearInterval(intervalId);
 });
 
-// Mostrar la primera imagen al cargar la página
-showSlide(currentSlide);
+slider.addEventListener("mouseleave", () => {
+    intervalId = setInterval(nextSlide, 3000);
+});
 
+// Inicializar el primer slide
+slides[slideIndex].classList.add("active");
